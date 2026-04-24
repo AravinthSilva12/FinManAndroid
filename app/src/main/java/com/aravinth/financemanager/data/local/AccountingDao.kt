a95183ea76d341c1fa6c0a4b990c538d8443b32e
Aravinthasamy <aravinthasamy281099@gmail.com>
2026-04-24 12:14:59 +0530

    April 11 : project setup - packages established; Navigation is half finished. April 12 : BottomNavigationBar is completed. April 13 : Navigation architecture is completed. Bottom Nav bar and screen switching is working good now. April 16 : Hilt dependencies added in gradle. April 22 : Data base design in progress*

--- app/src/main/java/com/aravinth/financemanager/data/local/AccountingDao.kt
+++ app/src/main/java/com/aravinth/financemanager/data/local/AccountingDao.kt
@@ -6,19 +6,19 @@ import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
-
 @Dao
 interface AccountingDao {
 
-     @Insert
-     suspend fun addTransactions(accounting: AccountingEntity)
+    @Insert
+    suspend fun addTransaction(item: AccountingEntity)
 
-     @Query("SELECT * FROM accounting_table ORDER BY timestamp ASC")
-     fun viewTransactions(): Flow<List<AccountingEntity>>
+    @Query("SELECT * FROM accounting_table ORDER BY timestamp DESC")
+    fun viewTransactions(): Flow<List<AccountingEntity>>
 
-     @Query("SELECT * FROM accounting_table WHERE type = :type")
-     fun viewByType(): Flow<List<AccountingEntity>>
+    @Query("SELECT * FROM accounting_table WHERE type = :type")
+    fun viewByType(type: String): Flow<List<AccountingEntity>>
 
-     @Delete
-     fun deleteTransaction(id: Int)
+    @Delete
+    suspend fun deleteTransaction(item: AccountingEntity)
+
 }
